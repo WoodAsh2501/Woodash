4d42e38d219948af29835a27f11f8ce758e99f77
Woodash <69113922+WoodAsh2501@users.noreply.github.com>
2024-01-30 23:47:29 +0800

    修改 文章 图片样式 JS使用轮询解决脚本执行时图片未加载的问题

--- scripts/article/setImageSize.js
+++ scripts/article/setImageSize.js
@@ -1,23 +1,47 @@
-let imgs = document.querySelectorAll("figure img");
+// 如果图片的load事件能正确触发的话就不用废这么大力气轮询了，感觉幽默
+
+const imgs = document.querySelectorAll("figure img");
 let sumRatio = 0;
-for (let i = 0; i < imgs.length; i++) {
-    const img = imgs[i];
-    let ratio = img.naturalWidth / img.naturalHeight;
-    sumRatio += ratio;
-}
-// 页面第一次加载好的时候重设图片宽度
-setImageSize();
-// 窗口大小变更时再次重设图片宽度
+let imgsLoaded = false;
+
+// 加载完成时重设尺寸
+let timer = setInterval(checkImagesLoaded, 500);
+
+// 窗口大小变更时重设图片尺寸
 window.addEventListener('resize', setImageSize);
 
+function checkImagesLoaded() {
+    let flag = true;
+    for (let i = 0; i < imgs.length; i++) {
+        const img = imgs[i];
+        if (!img.complete) {
+            flag = false;
+            break;
+        };
+        let ratio = img.naturalWidth / img.naturalHeight;
+        sumRatio += ratio;
+    }
+    imgsLoaded = (flag) ? true : false;  
+    console.log(sumRatio);
+
+    if (imgsLoaded) {
+        setImageSize();
+        clearInterval(timer);
+    }
+}
+
 function setImageSize() {
+    // 如果加载错误就不重设尺寸
+    if (sumRatio === 0) {
+        return
+    }
     let article = document.querySelector("article");
     let height = article.offsetWidth / sumRatio;
-    
+    // console.log(article.offsetWidth, sumRatio, height);
+
     for (let i = 0; i < imgs.length; i++) {
         const img = imgs[i];
         img.style.height = height + 'px';
+        img.style.width = "auto";
     }
-
-    console.log("已重设图片尺寸")
 }
